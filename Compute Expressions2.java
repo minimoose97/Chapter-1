ec7be62e01294bc2295d5430ec6345ef18e7c8a7
minimoose97 <50415614+minimoose97@users.noreply.github.com>
2019-05-09 10:06:08 -0600

    Add files via upload

---
+++ Compute Expressions2.java
@@ -0,0 +1,9 @@
+class ComputeExpressions {
+	public static void main(String[] args) {
+		System.out.println("area of rectangle is");
+		System.out.println(4.5 * 7.9);
+		System.out.println("perimiter of rectangle is");
+		System.out.println((4.5 * 2) + (7.9 * 2));
+		
+	}
+}
